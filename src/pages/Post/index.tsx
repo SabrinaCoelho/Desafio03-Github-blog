e2297014373ad1e2ea37b407d748fdd024f7bc72
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-12-15 23:24:57 -0300

    Fix load post page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import { PostTitle } from "./PostTitle";
 import { PostContainer, PostContent } from "./style";
 import { RepoContext } from "../../Contexts/RepoContext";
-import { useCallback, useContext, useEffect } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import Markdown from "react-markdown";
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
@@ -14,6 +14,7 @@ import { unified } from "unified"; */
 export function Post(){
     const {getRepoIssueData, actualIssue} = useContext(RepoContext);
     const {issueId} = useParams();
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(
       () => {
@@ -22,45 +23,42 @@ export function Post(){
           console.log(issueId)
           async function batata (issueId: string){
             await getRepoIssueData(issueId);
+            setLoaded(true);
           }
           batata(issueId);
         }else{
           console.log("iride");
         }
-      }, []
+      }, [issueId, getRepoIssueData]
     );
-
-    /* const handleLoadData = useCallback(
-      async (issueId: string) => {
-        await getRepoIssueData(issueId);
-      }, [getRepoIssueData]
-    ); */
     
-    return(
-        <PostContainer>
-            <PostTitle />
-              <PostContent className="postContainer">
-                <Markdown
-                  children={actualIssue.body}
-                  components={{
-                  code(props) {
-                    const {children} = props
-                    return (
-                      <SyntaxHighlighter
-                        PreTag="div"
-                        children={String(children).replace(/\n$/, '')}
-                        language="javascript"
-                        style={coldarkDark}
-                      />
-                    ) 
-                  },
-                  img(props) {
-                    const {node, ...rest} = props;
-                    return <img alt="" style={{width: "100%"}} src={rest.src}/>
-                  }
-                }}
-                />
-            </PostContent>
-        </PostContainer>
-    );
+    return loaded ? (
+      <PostContainer>
+        <PostTitle />
+          <PostContent className="postContainer">
+            <Markdown
+              children={actualIssue.body}
+              components={{
+              code(props) {
+                const {children} = props
+                return (
+                  <SyntaxHighlighter
+                    PreTag="div"
+                    children={String(children).replace(/\n$/, '')}
+                    language="javascript"
+                    style={coldarkDark}
+                  />
+                ) 
+              },
+              img(props) {
+                const {node, ...rest} = props;
+                return <img alt="" style={{width: "100%"}} src={rest.src}/>
+              }
+            }}
+            />
+          </PostContent>
+      </PostContainer>
+    ): (
+      <p>Carregando</p>
+    )
 }
